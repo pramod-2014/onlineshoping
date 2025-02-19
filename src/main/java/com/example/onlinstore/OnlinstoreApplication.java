9d15c4d64a8e60cbb1b523a115a1d41dc0405bb9
pramod kumar <prmd777@rediffmail.com>
2025-02-19 17:15:18 +0530

    online shopping code

---
+++ src/main/java/com/example/onlinstore/OnlinstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinstore/OnlinstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
